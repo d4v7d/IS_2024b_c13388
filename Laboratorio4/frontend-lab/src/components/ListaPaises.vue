2fbc5b9e8cc74051ef93b9e645553bb74224dc16
d4v7d <DAVID.GONZALEZVILLANUEVA@ucr.ac.cr>
2024-11-09 18:36:06 -0600

    Laboratorio 7

--- Laboratorio4/frontend-lab/src/components/ListaPaises.vue
+++ Laboratorio4/frontend-lab/src/components/ListaPaises.vue
@@ -1,80 +1,75 @@
 <template>
-    <div class="container mt-5">
-        <h1 class="display-4 text-center"> Lista de Paises </h1>
-        <div class="row justify-content-end">
-            <div class="col-2">
-                <a href="/pais">
-                    <button type="button" class="btn btn-outline-secondary float-right">
-                        Agregar pais
-                    </button>
-                </a>
-            </div>
-        </div>
+  <div class="container mt-5">
+    <h1 class="display-4 text-center"> Lista de Paises </h1>
+    <div class="row justify-content-end">
+      <div class="col-2">
+        <a href="/pais">
+          <button type="button" class="btn btn-outline-secondary float-right">
+            Agregar pais
+          </button>
+        </a>
+      </div>
     </div>
+  </div>
 
-    <div class="container mt-5">
-        <h1 class="display-4 text-center">Lista de países</h1>
-        <table
-        class="table is-bordered is-striped is-narrow is-hoverable
-        is-fullwidth"
-        >
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Continente</th>
-                    <th>Idioma</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(pais, index) of paises" :key="index">
-                    <td>{{ pais.nombre }}</td>
-                    <td>{{ pais.continente }}</td>
-                    <td>{{ pais.Idioma }}</td>
-                    <td>
-                        <button class="btn btn-secondary btn-sm">Editar</button>
-                        <button class="btn btn-danger btn-sm" @click="eliminar(index)">Eliminar</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+  <div class="container mt-5">
+    <table class="table is-bordered is-striped is-narrow is-hoverable
+        is-fullwidth">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Continente</th>
+          <th>Idioma</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(pais, index) of paises" :key="index">
+          <td>{{ pais.nombre }}</td>
+          <td>{{ pais.continente }}</td>
+          <td>{{ pais.idioma }}</td>
+          <td>
+            <button class="btn btn-secondary btn-sm">Editar</button>
+            <button class="btn btn-danger btn-sm" @click="eliminar(index)">Eliminar</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-    import axios from "axios";
-    export default {
-        name: 'ListaPaises',
-        data() {
-            return {
-                paises: [
-                    { nombre: "Costa Rica", continente: "América", Idioma: "Español" },
-                    { nombre: "Japón", continente: "Asia", Idioma: "Japonés" },
-                    { nombre: "Corea del Sur", continente: "Asia", Idioma: "Coreano" },
-                    { nombre: "Italia", continente: "Europa", Idioma: "Italiano" },
-                    { nombre: "Alemania", continente: "Europa", Idioma: "Alemán" },
-                ],
-            };
-        },
-        methods: {
-            eliminar(index) {
-                this.paises.splice(index, 1);
-            },
-            obtenerTareas() {
-                axios.get("https://localhost:7294/api/Paises").then(
-                (response) => {
-                    this.paises = response.data;
-                });
-            },
-            
-        },
-        created: function () {
-                this.obtenerTareas();
-            },
+import axios from "axios";
+export default {
+  name: 'ListaPaises',
+  data() {
+    return {
+      paises: [
+        { nombre: "Costa Rica", continente: "América", Idioma: "Español" },
+        { nombre: "Japón", continente: "Asia", Idioma: "Japonés" },
+        { nombre: "Corea del Sur", continente: "Asia", Idioma: "Coreano" },
+        { nombre: "Italia", continente: "Europa", Idioma: "Italiano" },
+        { nombre: "Alemania", continente: "Europa", Idioma: "Alemán" },
+      ],
     };
-    
+  },
+  methods: {
+    eliminar(index) {
+      this.paises.splice(index, 1);
+    },
+    obtenerTareas() {
+      axios.get("https://localhost:7294/api/Paises").then(
+        (response) => {
+          this.paises = response.data;
+        });
+    },
+
+  },
+  created: function () {
+    this.obtenerTareas();
+  },
+};
+
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
---
+++ Laboratorio7/UITestLab7/UIAutomationTests/Selenium.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace UIAutomationTests
+{
+    internal class Selenium
+    {
+        IWebDriver _driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Enter_To_List_Of_Countries_Test()
+        {
+            var URL = "http://localhost:8080/";
+
+            _driver.Navigate().GoToUrl(URL);
+
+            IWebElement pageTitle = _driver.FindElement(By.XPath("//h1[contains(text(), 'Lista de Paises')]"));
+            Assert.IsNotNull(pageTitle, "The page title 'Lista de Paises' was not found.");
+        }
+
+        [Test]
+        public void Navigate_To_Create_Country_Form_Test()
+        {
+            var URL = "http://localhost:8080/";
+            _driver.Navigate().GoToUrl(URL);
+
+            IWebElement agregarPaisLink = _driver.FindElement(By.XPath("//a[@href='/pais']"));
+            agregarPaisLink.Click();
+
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => driver.FindElement(By.Id("name")).Displayed);
+
+            IWebElement nombreField = _driver.FindElement(By.Id("name"));
+            IWebElement continenteField = _driver.FindElement(By.Id("continente"));
+            IWebElement idiomaField = _driver.FindElement(By.Id("idioma"));
+
+            Assert.IsTrue(nombreField.Displayed, "Nombre field is not displayed.");
+            Assert.IsTrue(continenteField.Displayed, "Continente field is not displayed.");
+            Assert.IsTrue(idiomaField.Displayed, "Idioma field is not displayed.");
+        }
+
+        [Test]
+        public void Create_New_Country_Test()
+        {
+            var URL = "http://localhost:8080/";
+            _driver.Navigate().GoToUrl(URL);
+
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+
+            IWebElement agregarPaisLink = _driver.FindElement(By.XPath("//a[@href='/pais']"));
+            agregarPaisLink.Click();
+
+            wait.Until(driver => driver.FindElement(By.Id("name")).Displayed);
+
+            string countryName = "TestCountry" + DateTime.Now.Ticks;
+            _driver.FindElement(By.Id("name")).SendKeys(countryName);
+
+            IWebElement continenteSelectElement = _driver.FindElement(By.Id("continente"));
+            SelectElement continenteSelect = new SelectElement(continenteSelectElement);
+            continenteSelect.SelectByText("Europa");
+
+            _driver.FindElement(By.Id("idioma")).SendKeys("TestIdioma");
+
+            _driver.FindElement(By.XPath("//button[contains(text(),'Guardar')]")).Click();
+
+            wait.Until(driver => driver.Url == URL);
+
+            bool isCountryPresent = _driver.FindElements(By.XPath($"//td[contains(text(),'{countryName}')]")).Count > 0;
+            Assert.IsTrue(isCountryPresent, "The new country was not added to the list.");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+    }
+}
